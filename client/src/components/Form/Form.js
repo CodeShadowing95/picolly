dacbd9293a349895665d8ac6f38f876dfa948bb2
Frank Patrick Namegni <CodeShadowing95>
2023-04-06 09:21:41 +0100

    Initializing project

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import useStyles from './styles';
+
+const Form = () => {
+  const classes = useStyles();
+  return (
+    <div>Form</div>
+  );
+}
+
+export default Form;
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Post from './Post/Post';
+
+import useStyles from './styles';
+
+const Posts = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <div>Posts</div>
+      <Post />
+      <Post />
+    </>
+  );
+}
+
+export default Posts;
