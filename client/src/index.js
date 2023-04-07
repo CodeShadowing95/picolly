a22878f2caf5c88d09a793881864fa890502c33a
Frank Patrick Namegni <CodeShadowing95>
2023-04-07 11:19:29 +0100

    create reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import reducers from './reducers';
 
 import App from './App';
 
+const store = configureStore({
+  reducer: reducers
+});
+
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
