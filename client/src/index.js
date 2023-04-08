6f752873daefc2754045c2fb275204a3d5ecb7ed
Frank Patrick Namegni <CodeShadowing95>
2023-04-08 09:37:21 +0100

    FEAT: Display a created post, styling the background

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import reducers from './reducers';
 
 import App from './App';
+import './index.css';
 
 const store = configureStore({
   reducer: reducers
