4753d6c949bbb1cb808986e0b51826dc8caccf9e
Frank Patrick Namegni <CodeShadowing95>
2023-04-10 04:25:19 +0100

    FEAT: Add actionTypes for our CRUD

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,14 +1,15 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+
 export const reducer =  (posts = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
+    case FETCH_ALL:
       return action.payload;
-    case 'CREATE':
+    case CREATE:
       return [...posts, action.payload];
-    case 'UPDATE':
-    case 'LIKE':
+    case UPDATE:
       // action.payload is here the newly updated post or memory
       return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-    case 'DELETE':
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
     default:
       return posts;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,10 +16,7 @@ app.use(cors());
 // Every routes inside the postRoutes will gonna start with 'posts'
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vastolorde:vastolorde123@cluster0.4dlauve.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
-
 // To connect to the database
-mongoose.connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL)
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`)))
   .catch((error) => console.log(error.message));
