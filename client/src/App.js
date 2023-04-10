6239b9c29a69be330760567098f2444021d1e9ed
Frank Patrick Namegni <CodeShadowing95>
2023-04-10 15:48:07 +0100

    ADD-ONS: Toolbar added, new font added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { ThemeProvider, Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import Navbar from './components/Navbar/Navbar';
+// To define a new font family
+import themeFont from './theme/themeFont';
 
 const App = () => {
   const [currentId, setCurrentId] = useState(null);
@@ -16,22 +18,25 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Container maxWidth="lg">
-      <Navbar />
+    // Define "Montserrat" font as font family' gloabal page
+    <ThemeProvider theme={themeFont}>
+      <Container maxWidth="lg">
+        <Navbar />
 
-      <Grow in>
-        <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
+        <Grow in>
+          <Container>
+            <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts setCurrentId={setCurrentId} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+          </Container>
+        </Grow>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/theme/themeFont.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const themeFont = createTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+  },
+});
+
+export default themeFont;
